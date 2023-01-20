e77e0d1c64324c37f3764d0ab279f30454ebfa4d
mohit6481 <mohit.rathi6481@gmail.com>
2023-01-20 17:56:06 +0530

    Add User Module Practice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
+import React, {useState} from "react";
+import AddUsers from "./components/Users/AddUsers";
+import UserList from "./components/Users/UserList";
 
-function App() {
-  return (
-    <div>
-      Welcome Mohit
-    </div>
-  );
+const DUMMY_USERS = [{
+  id: '1',
+  name: 'Mohit',
+  age: '28'
+}]
+
+const App = () => {
+  const [users, setUsers] = useState(DUMMY_USERS);
+  const addUserHandler = (user) => {
+    setUsers((prevState) => [user, ...prevState])
+  }
+  return <div>
+    <AddUsers onAddUser={addUserHandler} />
+    <UserList users={users}/>
+  </div>;
 }
 
 export default App;
---
+++ src/components/UI/ErrorModal.js
@@ -0,0 +1,24 @@
+import Button from "./Button";
+import Card from "./Card";
+import styles from "./ErrorModal.module.css";
+
+const ErrorModal = ({ title, message, onConfirm }) => {
+  return (
+    <div>
+      <div className={styles.backdrop} onClick={onConfirm} />
+      <Card className={styles.modal}>
+        <header className={styles.header}>
+          <h2>{title}</h2>
+        </header>
+        <div className={styles.content}>
+          <p>{message}</p>
+        </div>
+        <footer className={styles.actions}>
+          <Button onClick={onConfirm}>Okay</Button>
+        </footer>
+      </Card>
+    </div>
+  );
+};
+
+export default ErrorModal;
