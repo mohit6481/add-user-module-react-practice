b2ae4d906e20f547b468bae62ca5d1d8c9d64a40
mohit6481 <mohit.rathi6481@gmail.com>
2023-01-20 19:13:26 +0530

    Use refs to manipulate input

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -1,41 +1,50 @@
 import Card from "../UI/Card";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Button from "../UI/Button";
 import classes from "./AddUsers.module.css";
 import ErrorModal from "../UI/ErrorModal";
 
 const AddUsers = ({ onAddUser }) => {
-  const [enteredUserName, setEnteredUserName] = useState("");
-  const [enteredAge, setEnteredAge] = useState("");
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
+  // const [enteredUserName, setEnteredUserName] = useState("");
+  // const [enteredAge, setEnteredAge] = useState("");
   const [error, setError] = useState();
 
-  const userNameChangeHandler = (event) => {
-    setEnteredUserName(event.target.value);
-  };
+  // const userNameChangeHandler = (event) => {
+  //   setEnteredUserName(event.target.value);
+  // };
 
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
-  };
+  // const ageChangeHandler = (event) => {
+  //   setEnteredAge(event.target.value);
+  // };
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0) {
+    const enteredName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
       setError({
         title: "Invalid input",
         message: "Please enter a valid name and age (non-empty values).",
       });
       return;
     }
-    if (+enteredAge < 1) {
+    if (+enteredUserAge < 1) {
       setError({
         title: "Invalid age",
         message: "Please enter a valid age (>0.)",
       });
       return;
     }
-    onAddUser({ name: enteredUserName, age: enteredAge, id: Math.random() });
-    setEnteredUserName("");
-    setEnteredAge("");
+    onAddUser({ name: enteredName, age: enteredUserAge, id: Math.random() });
+
+    // Shouldnt use REF to manipulate DOM in large project, but its okay if this is small change and should be done rarely
+    nameInputRef.current.value = '';
+    ageInputRef.current.value = '';
+    // setEnteredUserName("");
+    // setEnteredAge("");
   };
 
   const errorHandler = () => {
@@ -51,15 +60,17 @@ const AddUsers = ({ onAddUser }) => {
           <input
             id="username"
             type="text"
-            onChange={userNameChangeHandler}
-            value={enteredUserName}
+            // onChange={userNameChangeHandler}
+            // value={enteredUserName}
+            ref={nameInputRef}
           />
           <label htmlFor="age">Age (Years)</label>
           <input
             id="age"
             type="number"
-            onChange={ageChangeHandler}
-            value={enteredAge}
+            // onChange={ageChangeHandler}
+            // value={enteredAge}
+            ref={ageInputRef}
           />
           <Button type="submit">Add User</Button>
         </form>
